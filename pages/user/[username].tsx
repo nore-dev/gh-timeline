2f841f2815b584ef0a72d6c0aa063fc7db23c588
noredev <noredeveloper@gmail.com>
2022-01-21 23:56:57 +0300

    feat: add timeline page

---
+++ pages/user/[username].tsx
@@ -0,0 +1,42 @@
+
+import axios from "../../lib/axios"
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+const Timeline: NextPage = () => {
+    const { username } = useRouter().query
+    const [user, setUser] = useState<any>({})
+
+    useEffect(() => {
+        if (!username) return
+        axios.get(`users/${username}`).then((res) => {
+            setUser(res.data)
+        })
+    }, [username])
+
+    return (
+    <div className="text-center">
+      <Head>
+        <title>@{username}</title>
+        <meta name="description" content="Description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+        <div>
+        <img width={150} height={150} alt="profile_photo" className="rounded" src={user.avatar_url}></img>
+        <h1>{user.login}</h1>
+        <h3>{user.bio}</h3>
+        <div>
+        <h3>{user.followers}</h3><p>Followers</p>
+        <h3>{user.following}</h3><p>Following</p>
+        </div>
+
+
+        </div>
+    </div>
+  )
+}
+
+export default Timeline
